6799a6cb56875a2774baa6c907231a5225c4fdc4
Fulbabu <fulbabucse@gmail.com>
2023-01-08 00:09:32 +0600

    footer

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
 import Navbar from "../Pages/Shared/Navbar";
 
 const Layout = () => {
@@ -9,6 +10,7 @@ const Layout = () => {
       <div className="max-w-screen-xl mx-auto">
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 };
